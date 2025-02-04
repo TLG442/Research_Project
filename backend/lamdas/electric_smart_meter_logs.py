f67bb48a6ded2a2cb4279b21863a64c828bbefd3
Thejan K.G.T <99307599+IT21222740@users.noreply.github.com>
2025-02-04 17:37:16 +0530

    implement functions for the post smart meter logs

---
+++ backend/lamdas/electric_smart_meter_logs.py
@@ -0,0 +1,63 @@
+import boto3
+import json
+import logging
+from electric_custom_encoder import CustomEncoder
+from boto3.dynamodb.conditions import Key
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+dyanamodb_table_name = 'smart-meter-logs'
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(dyanamodb_table_name)
+
+get_Method = 'GET'
+post_Method = 'POST'
+health_check_path = '/health'
+smart_meter = '/smart-meter'
+
+
+def lamda_handler(event,context):
+    logger.info(f"Received event: {json.dumps(event)}")
+    http_method = event['httpMethod']
+    path = event['path']
+
+    if http_method == get_Method and path == health_check_path:
+        response =  buildResponse(200)
+    elif http_method == get_Method and path == smart_meter:
+        response = getSmartMeterLogs(event['queryStringParameters']['meterId'])
+    elif http_method == post_Method and path == smart_meter:
+        response = saveLogs(json.loads(event['body']))
+    else:
+        response = buildResponse(404)
+
+    return response
+
+
+def buildResponse(statusCode,body=None):
+    response = {
+        'statusCode': statusCode,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    if body is not None:
+        response['body'] = json.dumps(body, cls=CustomEncoder)
+    return response
+
+def saveLogs(requestBody):
+    try:
+        table.put_item(Item=requestBody)
+        body = {
+            'Operation': 'SAVE',
+            'Message': 'Successfully saved logs',
+            'Log': requestBody
+        }
+        return buildResponse(201,body)
+    except:
+        logger.exception('Error saving logs')
+        return buildResponse(500,{'Operation': 'SAVE', 'Message': 'Failed to save logs'})
+    
+
