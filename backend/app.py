0694950d8fa29c39e9c104476863795bfda2a554
Tharuka Lakshan <tharukal@savinda.lk>
2025-02-05 12:46:55 +0530

    added water flow data endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from routes import log_data, get_accumulated_data, get_daily_consumption, predict
+from routes import log_data, get_accumulated_data, get_daily_consumption, predict , log_water_usage
 
 app = Flask(__name__)
 
@@ -8,6 +8,6 @@ app.add_url_rule('/log', 'log_data', log_data, methods=['POST'])
 app.add_url_rule('/accumulate', 'get_accumulated_data', get_accumulated_data, methods=['GET'])
 app.add_url_rule('/accumulate/<date>', 'get_daily_consumption', get_daily_consumption, methods=['GET'])
 app.add_url_rule('/predict', 'predict', predict, methods=['POST'])
-
+app.add_url_rule('/log_water', 'log_water_usage', log_water_usage, methods=['POST'])
 if __name__ == '__main__':
     app.run(debug=True)
