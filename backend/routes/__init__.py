1cf5484394df9d98eb425ed39c5d88eac3f93485
Tharuka Lakshan <tharukal@savinda.lk>
2025-03-16 11:37:32 +0530

    added pressure classify endpoint

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -5,3 +5,4 @@ from .electric_daily_power_consumption import get_daily_consumption
 from .electric_predict import predict
 from .water_flow import log_water_usage
 from .get_water_flow import get_water_usage
+from .pressure_classify import classify_leak
